74490546f3837f2fb925214a82af1b3943ddc6b1
kaferok <nikolai.verbitskij1.1.1@gmail.com>
2022-06-01 16:44:37 +0300

    added bottom navigation (#8)

--- app/src/main/java/com/bijov1apps/gurtamtest/activities/MainActivity.kt
+++ app/src/main/java/com/bijov1apps/gurtamtest/activities/MainActivity.kt
@@ -3,13 +3,20 @@ package com.bijov1apps.gurtamtest.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bijov1apps.gurtamtest.R
 import com.bijov1apps.gurtamtest.common.ActivityBinder
+import com.bijov1apps.gurtamtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), ActivityBinder {
+class MainActivity : AppCompatActivity(R.layout.activity_main), ActivityBinder {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.bottomNavigation.setupWithNavController(findNavController(R.id.mainContainer))
     }
 
     override fun toolbar(isVisible: Boolean, isShowIcon: Boolean, title: String?) {
